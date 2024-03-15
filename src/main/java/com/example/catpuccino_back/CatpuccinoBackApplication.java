2513e68c2ece19a31a6b455d1a1520ac3300ba29
Antonio López <tonidiez17@gmail.com>
2024-03-15 12:53:11 +0100

    subida

---
+++ src/main/java/com/example/catpuccino_back/CatpuccinoBackApplication.java
@@ -0,0 +1,14 @@
+package com.example.catpuccino_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatpuccinoBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatpuccinoBackApplication.class, args);
+	}
+
+z
+}
