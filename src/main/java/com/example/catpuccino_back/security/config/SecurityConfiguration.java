cc7856dcc368cb9439fcd010da73026429acb16f
SaraUrban1 <sprietourban@safareyes.es>
2024-03-27 12:32:16 +0100

    Sara - Carrito

--- src/main/java/com/example/catpuccino_back/security/config/SecurityConfiguration.java
+++ src/main/java/com/example/catpuccino_back/security/config/SecurityConfiguration.java
@@ -53,6 +53,7 @@ public class SecurityConfiguration {
                                 .requestMatchers("/auth/**").permitAll()
                                 .requestMatchers("/Admin/**").hasAnyAuthority(Rol.SUPERUSER.name())
                                 .requestMatchers("/User/**").hasAnyAuthority(Rol.USUARIO.name())
+                                .requestMatchers("/consCumiciones/**").permitAll()
                                 .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
--- src/main/java/com/example/catpuccino_back/service/ConsumicionService.java
+++ src/main/java/com/example/catpuccino_back/service/ConsumicionService.java
@@ -1,7 +1,11 @@
 package com.example.catpuccino_back.service;
 
 import com.example.catpuccino_back.converter.ConsumicionMapper;
+import com.example.catpuccino_back.converter.ProductoMapper;
+import com.example.catpuccino_back.converter.ReservaMapper;
 import com.example.catpuccino_back.dto.ConsumicionDTO;
+import com.example.catpuccino_back.dto.ProductoDTO;
+import com.example.catpuccino_back.dto.ReservaDTO;
 import com.example.catpuccino_back.models.Consumicion;
 import com.example.catpuccino_back.models.Producto;
 import com.example.catpuccino_back.models.Reserva;
@@ -10,9 +14,11 @@ import com.example.catpuccino_back.repository.ProductoRepository;
 import com.example.catpuccino_back.repository.ReservaRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PutMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -29,7 +35,19 @@ public class ConsumicionService {
     private ReservaRepository reservaRepository;
 
     @Autowired
+    private ReservaService reservaService;
+
+    @Autowired
+    private ReservaMapper reservaMapper;
+
+    @Autowired
     private ProductoRepository productoRepository;
+
+    @Autowired
+    private ProductoService productoService;
+
+    @Autowired
+    private ProductoMapper productoMapper;
 
 
     //LISTAR
@@ -53,7 +71,7 @@ public class ConsumicionService {
         Consumicion consumicion = consumicionRepository.findById(consumicionDTO.getId()).orElse(null);
         if (consumicion == null) {
             return null;
-        }else{
+        } else {
             Reserva reserva = reservaRepository.findById(consumicionDTO.getReservaDTO().getId()).orElse(null);
             Producto producto = productoRepository.findById(consumicionDTO.getProductoDTO().getId()).orElse(null);
             consumicion.setCantidad(consumicionDTO.getCantidad());
@@ -66,13 +84,39 @@ public class ConsumicionService {
     }
 
     //BORRAR
-    public String eliminarConsumicion(Integer id){
-        if(id==null){
-            return("No se ha encontrado la consumicion");
-        }else{
+    public String eliminarConsumicion(Integer id) {
+        if (id == null) {
+            return ("No se ha encontrado la consumicion");
+        } else {
             consumicionRepository.deleteById(id);
-            return("Consumicion eliminada");
+            return ("Consumicion eliminada");
         }
     }
+
+    //CARRITO
+    private List<ConsumicionDTO> carrito = new ArrayList<>();
+
+    public void agregarProductoAlCarrito(Integer idProducto, Integer cantidad, Integer idReserva) {
+        Producto producto = productoService.getByIdProducto(idProducto);
+        ProductoDTO productoDTO = productoMapper.toDTO(producto);
+        Reserva reserva = reservaService.getByIdReserva(idReserva);
+        ReservaDTO reservaDTO = reservaMapper.toDTO(reserva);
+
+        if (productoDTO != null && reservaDTO != null) {
+            ConsumicionDTO consumicion = ConsumicionDTO.builder()
+                    .productoDTO(productoDTO)
+                    .cantidad(cantidad)
+                    .reservaDTO(reservaDTO)
+                    .build();
+
+            Consumicion consumicionEntity = consumicionMapper.toEntity(consumicion);
+            consumicionRepository.save(consumicionEntity);
+
+            carrito.add(consumicion);
+        }
+    }
+
+
+
 
 }
